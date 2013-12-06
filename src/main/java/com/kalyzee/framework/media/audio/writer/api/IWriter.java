74cfb245103ebec2633d3339dd4dde802e655332
Ludovic Bouguerra <bouguerra.ludovic@gmail.com>
2013-12-06 08:19:40 +0100

    Mise à jour du modele writer

--- src/main/java/com/kalyzee/framework/media/audio/writer/api/IWriter.java
+++ src/main/java/com/kalyzee/framework/media/audio/writer/api/IWriter.java
@@ -2,8 +2,10 @@ package com.kalyzee.framework.media.audio.writer.api;
 
 import java.io.OutputStream;
 
+import com.kalyzee.framework.media.audio.api.IAudio;
+
 public interface IWriter {
 	
-	void write(OutputStream outputStream);
+	void write(OutputStream outputStream, IAudio audio);
 	boolean isCompatible(String extensionFormat);
 }
--- src/main/java/com/kalyzee/framework/media/audio/writer/impl/WavWriter.java
+++ src/main/java/com/kalyzee/framework/media/audio/writer/impl/WavWriter.java
@@ -2,11 +2,12 @@ package com.kalyzee.framework.media.audio.writer.impl;
 
 import java.io.OutputStream;
 
+import com.kalyzee.framework.media.audio.api.IAudio;
 import com.kalyzee.framework.media.audio.writer.api.IWriter;
 
 public class WavWriter extends AbstractWriter implements IWriter{
 
-	public void write(OutputStream outputStream) {
+	public void write(OutputStream outputStream, IAudio audio) {
 		
 	}
 
